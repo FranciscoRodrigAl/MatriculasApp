3997422240b40e73622e7d77dd1bba00353178fc
MelissaMF <melissa.munoz1@virginiogomez.cl>
2023-11-19 00:29:55 -0300

    DAL

---
+++ DAL/AlumnoDAL.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class AlumnoDAL
+    {
+        private string _connectionString = "data source=MELISSAMF\\SQLEXPRESS;initial catalog=MatriculaApp;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+        public bool GuardarAlumno(Alumno alumno)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Alumno(FechaIngreso, CarreraActual, EstadoAcademico, PersonaId) VALUES (@FechaIngreso, @CarreraActual, @EstadoAcademico, @PersonaId)", con))
+                {
+                    cmd.Parameters.AddWithValue("@FechaIngreso", alumno.FechaIngreso);
+                    cmd.Parameters.AddWithValue("@CarreraActual", alumno.CarreraActual);
+                    cmd.Parameters.AddWithValue("@EstadoAcademico", alumno.EstadoAcademico);
+                    cmd.Parameters.AddWithValue("@PersonaId", alumno.PersonaId);
+                    int rows = cmd.ExecuteNonQuery();
+                    return rows > 0;
+                }
+            }
+        }
+
+        public Alumno ObtenerAlumnoPorId(int id)
+        {
+            Alumno alumno = null;
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Alumno WHERE AlumnoId = @id", con))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            alumno = new Alumno
+                            {
+                                AlumnoId = Convert.ToInt32(reader["AlumnoId"]),
+                                FechaIngreso = Convert.ToDateTime(reader["FechaIngreso"]),
+                                CarreraActual = reader["CarreraActual"].ToString(),
+                                EstadoAcademico = reader["EstadoAcademico"].ToString(),
+                                PersonaId = Convert.ToInt32(reader["PersonaId"])
+                            };
+                        }
+                    }
+                }
+            }
+
+            return alumno;
+        }
+
+        public List<Alumno> ObtenerAlumnos()
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Alumno", con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Alumno alumno = new Alumno
+                            {
+                                AlumnoId = Convert.ToInt32(reader["AlumnoId"]),
+                                FechaIngreso = Convert.ToDateTime(reader["FechaIngreso"]),
+                                CarreraActual = reader["CarreraActual"].ToString(),
+                                EstadoAcademico = reader["EstadoAcademico"].ToString(),
+                                PersonaId = Convert.ToInt32(reader["PersonaId"])
+                            };
+
+                            alumnos.Add(alumno);
+                        }
+                    }
+                }
+            }
+
+            return alumnos;
+        }
+
+        public bool EliminarAlumno(int id)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Alumno WHERE AlumnoId = @id", con))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    int rows = cmd.ExecuteNonQuery();
+                    return rows > 0;
+                }
+            }
+        }
+    }
+}
---
+++ DAL/MatriculaDAL.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class MatriculaDAL
+    {
+        private string _connectionString = "data source=MELISSAMF\\SQLEXPRESS;initial catalog=MatriculaApp;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+        public bool GuardarMatricula(Matricula matricula)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Matricula(Codigo, Fecha, Carrera, AnioMatricula, SegmentoAcademico, AlumnoId) VALUES (@Codigo, @Fecha, @Carrera, @AnioMatricula, @SegmentoAcademico, @AlumnoId)", con))
+                {
+                    cmd.Parameters.AddWithValue("@Codigo", matricula.Codigo);
+                    cmd.Parameters.AddWithValue("@Fecha", matricula.Fecha);
+                    cmd.Parameters.AddWithValue("@Carrera", matricula.Carrera);
+                    cmd.Parameters.AddWithValue("@AnioMatricula", matricula.AnioMatricula);
+                    cmd.Parameters.AddWithValue("@SegmentoAcademico", matricula.SegmentoAcademico);
+                    cmd.Parameters.AddWithValue("@AlumnoId", matricula.AlumnoId);
+                    int rows = cmd.ExecuteNonQuery();
+                    return rows > 0;
+                }
+            }
+        }
+
+        public Matricula ObtenerMatriculaPorId(int id)
+        {
+            Matricula matricula = null;
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Matricula WHERE MatriculaId = @id", con))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            matricula = new Matricula
+                            {
+                                MatriculaId = Convert.ToInt32(reader["MatriculaId"]),
+                                Codigo = reader["Codigo"].ToString(),
+                                Fecha = Convert.ToDateTime(reader["Fecha"]),
+                                Carrera = reader["Carrera"].ToString(),
+                                AnioMatricula = Convert.ToInt32(reader["AnioMatricula"]),
+                                SegmentoAcademico = reader["SegmentoAcademico"].ToString(),
+                                AlumnoId = Convert.ToInt32(reader["AlumnoId"])
+                            };
+                        }
+                    }
+                }
+            }
+
+            return matricula;
+        }
+
+        public List<Matricula> ObtenerMatriculas()
+        {
+            List<Matricula> matriculas = new List<Matricula>();
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Matricula", con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Matricula matricula = new Matricula
+                            {
+                                MatriculaId = Convert.ToInt32(reader["MatriculaId"]),
+                                Codigo = reader["Codigo"].ToString(),
+                                Fecha = Convert.ToDateTime(reader["Fecha"]),
+                                Carrera = reader["Carrera"].ToString(),
+                                AnioMatricula = Convert.ToInt32(reader["AnioMatricula"]),
+                                SegmentoAcademico = reader["SegmentoAcademico"].ToString(),
+                                AlumnoId = Convert.ToInt32(reader["AlumnoId"])
+                            };
+
+                            matriculas.Add(matricula);
+                        }
+                    }
+                }
+            }
+
+            return matriculas;
+        }
+
+        public bool EliminarMatricula(int id)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Matricula WHERE MatriculaId = @id", con))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    int rows = cmd.ExecuteNonQuery();
+                    return rows > 0;
+                }
+            }
+        }
+
+    }
+}
